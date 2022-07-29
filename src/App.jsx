537527ef6523fcf7017cefa1ffadbc385ec92b45
Jose Acosta <acostajoma9@gmail.com>
2022-07-29 17:29:44 -0600

    Included a spinner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled'
 import imagenCripto from './img/imagen-criptos.png'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -46,17 +47,26 @@ function App() {
 
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0){
+
       const cotizarCripto = async () =>{
+        setCargando(true)
+        setResultado({})
+
         const {moneda, criptoMoneda} = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
         setResultado(resultado.DISPLAY[criptoMoneda][moneda])
+
+        setCargando(false)
       }
+
       cotizarCripto()
+
     }
   },[monedas])
 
@@ -71,6 +81,7 @@ function App() {
         <Formulario 
           setMonedas={setMonedas}
         />
+        {cargando && <Spinner />}
         {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
